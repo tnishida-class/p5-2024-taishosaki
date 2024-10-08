80dbbeb3491f7b958e84957e77d1784d84d31ff5
taishosaki <saki40519@icloud.com>
2024-10-08 11:24:48 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -6,7 +6,7 @@ function draw() {
   background(0);
   strokeWeight(4);
   fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
+  triangle(12,120,116,120,116,15);
   fill(94, 185, 84);
   triangle(0,112,110,112,110,0);
   fill(255);
